0331ae153be1a9515adc426d085e4cdc966338c4
davischristianson <dav.christian10@gmail.com>
2024-04-21 16:33:22 -0700

    Initial template website for project part 2

---
+++ Card-Creation-Website/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Card_Creation_Website.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
