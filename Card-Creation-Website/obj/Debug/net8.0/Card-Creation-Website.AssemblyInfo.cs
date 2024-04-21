f6c79bc6b6d145b88258746be3ecbc20d7f18846
davischristianson <dav.christian10@gmail.com>
2024-04-21 16:38:48 -0700

    Extra files generated from running the website once

--- Card-Creation-Website/obj/Debug/net8.0/Card-Creation-Website.AssemblyInfo.cs
+++ Card-Creation-Website/obj/Debug/net8.0/Card-Creation-Website.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Card-Creation-Website")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9bc52870cb5298dce1b9084495e191e351810c27")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1cab809d3e80337b0a00fc32248a9a530e0ffce6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Card-Creation-Website")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Card-Creation-Website")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
