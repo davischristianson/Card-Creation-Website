f20b50e18c814ae00c1f1391c29a27b3c99c6042
davischristianson <dav.christian10@gmail.com>
2024-04-22 13:21:28 -0700

    Added comments to Account class

--- Card-Creation-Website/Models/Account.cs
+++ Card-Creation-Website/Models/Account.cs
@@ -8,25 +8,49 @@ namespace Card_Creation_Website.Models
     /// </summary>
     public class Account
     {
+        /// <summary>
+        /// Main UserID auto generated
+        /// </summary>
         [Key]
         public int UserId { get; set; }
 
+        /// <summary>
+        /// Username for the user's account
+        /// </summary>
         [Required]
         public string Username { get; set; }
 
+        /// <summary>
+        /// Password for a user's account
+        /// </summary>
         [Required]
         public string Password { get; set; }
 
+        /// <summary>
+        /// User's first name
+        /// </summary>
         public string FirstName { get; set; }
 
+        /// <summary>
+        /// User's last name
+        /// </summary>
         public string LastName { get; set; }
 
+        /// <summary>
+        /// Email for the user's account
+        /// </summary>
         [Required]
         public string Email { get; set; }
 
+        /// <summary>
+        /// User's date of birth
+        /// </summary>
         [Required]
         public DateOnly DateOfBirth { get; set; }
 
+        /// <summary>
+        /// User's phone numberss
+        /// </summary>
         public string PhoneNumber { get; set; }
     }
 }
