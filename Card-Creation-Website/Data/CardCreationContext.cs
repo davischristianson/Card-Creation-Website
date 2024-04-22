9962fb0d3a1fa595cc4b5d7826773829bb5be84a
davischristianson <dav.christian10@gmail.com>
2024-04-22 10:37:01 -0700

    DbContext + extra commands added from when they were used in the eCommerceSite project

---
+++ Card-Creation-Website/Data/CardCreationContext.cs
@@ -0,0 +1,17 @@
+﻿using Card_Creation_Website.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Card_Creation_Website.Data
+{
+    public class CardCreationContext : DbContext
+    {
+        public CardCreationContext(DbContextOptions<CardCreationContext> options) : base(options) 
+        { 
+        
+        }
+
+        public DbSet<Card> Cards { get; set; }
+
+        public DbSet<Account> Accounts { get; set; }
+    }
+}
