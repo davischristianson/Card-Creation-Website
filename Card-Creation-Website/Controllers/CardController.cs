22ace2688815b18c3ac82359726d52177238ba66
davischristianson <dav.christian10@gmail.com>
2024-05-22 17:44:19 -0700

    Trying out some things to try and get navigation to work, renamed card related crud websites

---
+++ Card-Creation-Website/Controllers/CardController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Card_Creation_Website.Models;
+using Card_Creation_Website.Data;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace Card_Creation_Website.Controllers
+{
+    public class CardController : Controller
+    {
+        private readonly CardCreationContext _context;
+
+        public CardController(CardCreationContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> IndexCard()
+        {
+            List<Card> cards = await _context.Cards.ToListAsync();
+
+            return View(cards);
+        }
+
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Card cardToCreate)
+        {
+            if(ModelState.IsValid)
+            {
+                _context.Cards.Add(cardToCreate); // Prepares Insert
+                await _context.SaveChangesAsync();
+
+                ViewData["Message"] = $"{cardToCreate.CardName} was added successfully!";
+                return View();
+            }
+
+            return View(cardToCreate);
+        }
+    }
+}
