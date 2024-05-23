03e7a5f6b7f268fb44c283993a8057ec1305fe95
davischristianson <dav.christian10@gmail.com>
2024-05-23 15:26:13 -0700

    Added migration for card class

--- Card-Creation-Website/Migrations/20240523222421_inital2.Designer.cs
+++ Card-Creation-Website/Migrations/20240523222421_inital2.Designer.cs
@@ -4,6 +4,7 @@ using Card_Creation_Website.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Card_Creation_Website.Migrations
 {
     [DbContext(typeof(CardCreationContext))]
-    partial class CardCreationContextModelSnapshot : ModelSnapshot
+    [Migration("20240523222421_inital2")]
+    partial class inital2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
