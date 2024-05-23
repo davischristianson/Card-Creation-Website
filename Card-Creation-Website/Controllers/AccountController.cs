9aadd7511cc6cb53721831e49bda43168cd710c5
davischristianson <dav.christian10@gmail.com>
2024-05-22 18:33:08 -0700

    Added Login page as well and updated html on log in page, needs more work for navigation

---
+++ Card-Creation-Website/Controllers/AccountController.cs
@@ -0,0 +1,32 @@
+﻿using Azure.Identity;
+using Card_Creation_Website.Data;
+using Card_Creation_Website.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.Web.CodeGenerators.Mvc.Templates.BlazorIdentity.Pages.Manage;
+
+namespace Card_Creation_Website.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly CardCreationContext _context;
+
+        public AccountController(CardCreationContext context)
+        {
+            _context = context;
+        }
+
+
+        [HttpGet]
+        public IActionResult CreateAccount()
+        {
+            return View();
+        }
+
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
