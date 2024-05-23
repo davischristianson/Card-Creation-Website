e47ed294eae13e0337276a2549c061d3c307de08
davischristianson <dav.christian10@gmail.com>
2024-05-22 18:10:58 -0700

    Added login and registration models to the account class and updated the CreateAccount view

--- Card-Creation-Website/Models/Account.cs
+++ Card-Creation-Website/Models/Account.cs
@@ -55,4 +55,45 @@ namespace Card_Creation_Website.Models
         /// </summary>
         public string PhoneNumber { get; set; }
     }
+
+    public class RegisterViewModel
+    {
+        [Required]
+        [StringLength(50)]
+        public string Username { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [StringLength(100)]
+        public string Email { get; set; }
+
+        [Required]
+        [Compare(nameof(Email))]
+        [Display(Name = "Confirm Email")]
+        public string ConfirmEmail { get; set; }
+
+        [Required]
+        [StringLength(75, MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required]
+        [Compare(nameof(Password))]
+        [Display(Name = "Confirm Password")]
+        [DataType(DataType.Password)]
+        public string ConfirmPassword { get; set; }
+
+
+    }
+
+    public class LoginViewModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
 }
