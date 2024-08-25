f92e7166f1d02369596c4f413333281ab6fdf4b0
davischristianson <dav.christian10@gmail.com>
2024-08-25 12:08:24 -0700

    Added these two models to help with image uploading

---
+++ Card-Creation-Website/Models/CombinedModel.cs
@@ -0,0 +1,18 @@
+﻿namespace Card_Creation_Website.Models
+{
+    public class CombinedModel
+    {
+        /// <summary>
+        /// This model is the card model that will contain
+        /// the info for the respective model submitted on 
+        /// the Card Create form
+        /// </summary>
+        public Card CardModel { get; set; }
+
+        /// <summary>
+        /// This model is the image model that will contain
+        /// the image submitted on the Card Create Page
+        /// </summary>
+        public ImageModel ImageModel { get; set; }
+    }
+}
---
+++ Card-Creation-Website/Models/ImageModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Card_Creation_Website.Models
+{
+    public class ImageModel
+    {
+        /// <summary>
+        /// This will be to hold the image file for the
+        /// upload on the corresponding view
+        /// </summary>
+        public IFormFile ImageFile { get; set; }
+    }
+}
