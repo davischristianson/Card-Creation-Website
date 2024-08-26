d79c524460353a315cfe4088510f9122ff57c148
davischristianson <dav.christian10@gmail.com>
2024-08-26 06:54:42 -0700

    This is cleaning up the email template and commenting in future changes as well as some documentation

--- Card-Creation-Website/Models/IEmailProvider.cs
+++ Card-Creation-Website/Models/IEmailProvider.cs
@@ -17,6 +17,19 @@ namespace Card_Creation_Website.Models
             _config = config;
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="email"></param>
+        /// <param name="fromEmail"></param>
+        /// <param name="subject"></param>
+        /// <param name="content"></param>
+        /// <param name="htmlContent"></param>
+        /// Might need to add a parameter that takes in first and 
+        /// last name so the new email address can have the name on it
+        /// <returns></returns>
+        /// 
+        /// public async Task SendEmailAsync(string email, string fromEmail, string subject, string content, string htmlContent, string FullName)
         public async Task SendEmailAsync(string email, string fromEmail, string subject, string content, string htmlContent)
         {
             // SendGrid Email Test
@@ -24,11 +37,20 @@ namespace Card_Creation_Website.Models
             var client = new SendGridClient(apiKey);
             var msg = new SendGridMessage()
             {
+                // What a real email should look like
+                // Should grab the domain email, can hard code this preferably since this won't change very much
+                // From = new EmailAddress("domain@domain.com", "domain"),
+                // Subject = subject,
+                // PlainTextContent = content,
+                // HtmlContent = htmlContent
+
                 From = new EmailAddress("kejegamer@gmail.com", "KG Team"),
-                Subject = "Testing again, test test test",
-                PlainTextContent = "This is a test email",
-                HtmlContent = "<strong>Testing email test test</strong>"
+                Subject = subject,
+                PlainTextContent = content,
+                HtmlContent = htmlContent
             };
+            // Correct destination to the recipient
+            // msg.AddTo(new EmailAddress(email, FullName));
             msg.AddTo(new EmailAddress("kejegamer@gmail.com", "Keje"));
             await client.SendEmailAsync(msg);
             // var response = await client.SendEmailAsync(msg).ConfigureAwait(false);
